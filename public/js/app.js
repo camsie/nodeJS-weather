366f49880c1fbb352cf85294acabf58d8a4fa2c8
Mañalac <camille.manalac@accenture.com>
2021-08-04 15:14:26 +0800

    Setup

--- public/js/app.js
+++ public/js/app.js
@@ -21,7 +21,7 @@ weatherForm.addEventListener('submit', (e) => {
     if (location) {
         messageTwo.textContent = 'Loading...'
         messageOne.textContent = ''
-        fetch('http://localhost:3000/weather?address=' + location).then((response) => {
+        fetch('/weather?address=' + location).then((response) => {
     response.json().then((data) => {
         if (data.error) {
             //console.log(data.error)
